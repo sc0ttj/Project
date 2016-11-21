3b1f4e4ed142f1f39d05c085817d5185b5f4cf9b
sc0ttj <shout@scottjarvis.com>
2016-11-21 17:17:50 +0000

    cms will add inline tmpl <script> to page programmatically, remove hard-coded script from index.html

--- src/cms/js/cms.js
+++ src/cms/js/cms.js
@@ -8,15 +8,15 @@ var cms = {
     console.log('cms js initialised');
     $('body').addClass('with-js');
     // Do JSON + template HTML = output HTML
-    this.setTemplateExample('#template1');
-    this.setTemplateFromFileExample('#template2', "article-left");
-    this.setTemplateFrominlineScriptExample('#template3', "article-left");
-    this.setTemplateFromHtmlImportsExample('#template4', "article-left");
+    this.setTemplate_Example('#template1');
+    this.setTemplateFromFile_Example('#template2', "article-left");
+    this.setTemplateFrominlineScript_Example('#template3', "article-left");
+    this.setTemplateFromHtmlImports_Example('#template4', "article-left");
   },
 
   // Example methods below
 
-  setTemplateExample: function (elem){
+  setTemplate_Example: function (elem){
     var container = document.querySelector(elem);
     var data = {
       header: 'New Header Added by CMS',
@@ -28,25 +28,24 @@ var cms = {
     this.templater.render(container, data);
   },
 
-  setTemplateFromFileExample: function(elem, templateName){
+  setTemplateFromFile_Example: function(elem, tmplName){
     var self = this;
     var container = document.querySelector(elem);
-    var data = this.templater.getJsonFile(templateName, function (txt){
+    var data = this.templater.getJsonFile(tmplName, function (txt){
       var json = JSON.parse(txt);
       self.templater.render(container, json);
     });
   },
 
-  setTemplateFrominlineScriptExample: function(elem, templateName){
+  setTemplateFrominlineScript_Example: function(elem, tmplName){
+    this.addInlineTemplateToPage(tmplName);
     var self = this;
     var container = document.querySelector(elem);
-    var tmpl = document.getElementById(templateName + '-tmpl-script').innerHTML;
-
+    var tmpl = document.getElementById(tmplName + '-script').innerHTML;
     var wrapper = document.createElement('div');
     wrapper.innerHTML = tmpl;
-
     container.appendChild(wrapper);
-    var data = this.templater.getJsonFile(templateName, function (txt){
+    var data = this.templater.getJsonFile(tmplName, function (txt){
       var json = JSON.parse(txt);
       self.templater.render(container, json);
     });
@@ -54,12 +53,12 @@ var cms = {
 
   //https://www.html5rocks.com/en/tutorials/webcomponents/imports/
   //http://www.hongkiat.com/blog/html-import/
-  setTemplateFromHtmlImportsExample: function(elem, templateName){
+  setTemplateFromHtmlImports_Example: function(elem, tmplName){
     var self = this;
     var container = document.querySelector(elem);
-    var tmplFile = this.getTmplFromHMTLImport(templateName);
+    var tmplFile = this.getTmplFromHMTLImport(tmplName);
     container.appendChild(tmplFile);
-    var data = this.templater.getJsonFile(templateName, function (txt){
+    var data = this.templater.getJsonFile(tmplName, function (txt){
       var json = JSON.parse(txt);
       self.templater.render(container, json);
     });
@@ -67,15 +66,34 @@ var cms = {
 
   // helpers
 
-  getTmplFromHMTLImport: function (templateName) {
+  getTemplateHtmlString: function (tmplName) {
+    var html = '';
+    switch(tmplName){
+      case 'article-left':
+        html = '<div class="' + tmplName + '-tmpl-div"><h1 class="header"></h1><div class="paras"><p class="para"></p></div></div>';
+        break;
+    }
+    return html;
+  },
+
+  addInlineTemplateToPage: function(tmplName){
+    var html = this.getTemplateHtmlString(tmplName);
+    var script = document.createElement("script");
+    script.id = tmplName + '-script';
+    script.type = "text/tmpl"
+    script.innerHTML = html;
+    document.body.appendChild(script);
+  },
+
+  getTmplFromHMTLImport: function (tmplName) {
     var self = this;
     if (!self.hasHTMLImports()) {
       console.log('html imports will NOT work');
       // Use other libraries/require systems to load files.
       return false;
     }
-    var templateLink = document.querySelector('#' + templateName + '-tmpl');
-    var tmpl = templateLink.import.querySelector('.' + templateName + '-tmpl');
+    var templateLink = document.querySelector('#' + tmplName + '-tmpl');
+    var tmpl = templateLink.import.querySelector('.' + tmplName + '-tmpl');
     return tmpl;
   },
 
