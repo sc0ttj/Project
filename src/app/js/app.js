da4bc7651edd47711114889dfca460b5fbb58cdc
sc0ttj <shout@scottjarvis.com>
2016-12-05 21:30:06 +0000

    templates statically compiled into page at build time using handlebars

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -9,26 +9,10 @@ module.exports = {
     if (this.cutsTheMustard()){
       // add mustard
       $('body').addClass('html5');
-
       //load 'nice-to-haves'
       this.loadStylesheet('css/full.css');
       this.loadModules(['test', 'test1']);
-
-      // load cms with custom options
-      var cms = require('cms');
-      var cmsConfig = {
-        'name'            : 'custom options',
-        'templates'       : ['hero-center.tmpl', 'article-full-width.tmpl', 'stat-text.tmpl', 'image-center.tmpl', 'article-right.tmpl', 'image-fixed.tmpl', 'article-left.tmpl', 'youtube-full-width.tmpl', 'article-full-width.tmpl', 'scrollmation-text-left.tmpl', 'article-full-width.tmpl'  ],
-        'templatesDir'    : './templates/',
-        'sectionSelector' : 'body .section',
-        'sectionContainer': '<div class="section"></div>', 
-        'mustardClass'    : 'with-cms',
-      };
-      cms.init(cmsConfig);
-
-
     }
-
   },
 
   cutsTheMustard: function () {
--- src/cms/js/modules/templater.js
+++ src/cms/js/modules/templater.js
@@ -7,8 +7,10 @@ module.exports = {
   init: function(config){
     this.setConfig(config);
     
-    if (this.pageHasNoSections()) this.createSections();
-    
+    var sections = this.getSections();
+
+    sections.each(this.numberTheSections);
+
     return true // if we loaded ok
   },
 
@@ -20,59 +22,12 @@ module.exports = {
     this.config = config || this.config;
   },
 
-  pageHasNoSections: function(){
-    if (this.getSections.length < 1) return true;
-  },
-
   getSections: function(){
     return $(this.config.sectionSelector);
   },
 
-  createSections: function (){
-    var self = this;
-    function addSectionToPage(){
-      $('body').prepend(self.config.sectionContainer);
-    }
-    function numberTheSections(elem, i){
-      $(elem).addClass('section' + (i+1));
-    }
-    this.config.templates.forEach(addSectionToPage);
-    var sections = this.getSections();
-    sections.each(numberTheSections);
-    return sections;
-  },
-
-  getTemplates: function(){
-    return this.config.templates;
-  },
-
-  processTemplates: function (templates, templateProcessor){
-    var sections = $(this.config.sectionSelector);
-    for (var i=0; i<templates.length; i++){
-      var url = this.config.templatesDir + templates[i];
-      this.getTemplateFromUrl(url, templateProcessor, sections[i]);
-    }
-  },
-
-  getTemplateFromUrl: function(templateUrl, templateProcessor, container){
-    var opts = {url:templateUrl};
-    function ajaxResponder(respCode, html, xmlhttp){
-      if (respCode === 200) templateProcessor(html, container, templateUrl);
-    }
-    ajax.ajax(opts, ajaxResponder);
-  },
-
-  renderTemplate: function(templateHtml, container, templateUrl){
-    $(container).html(templateHtml);
-    var templateHasJs = (templateHtml.indexOf('<!-- js -->')> -1);
-
-    if (templateHasJs) {
-      var templateScript = templateUrl.substr(0, templateUrl.lastIndexOf(".")) + ".js";
-      var script  = document.createElement('script');
-      script.type = 'text/javascript'; 
-      script.src  = templateScript;
-      $('body').append(script);
-    }
+  numberTheSections: function(elem, i){
+    $(elem).addClass('section' + (i+1));
   },
 
 }
