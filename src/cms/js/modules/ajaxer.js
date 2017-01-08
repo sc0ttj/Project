a542747ec1309f070a3e39b69f66b6edd5585c87
sc0ttj <shout@scottjarvis.com>
2017-01-08 14:48:12 +0000

    cleanup in ajaxer

--- src/cms/js/modules/ajaxer.js
+++ src/cms/js/modules/ajaxer.js
@@ -1,20 +1,21 @@
+var xhr;
+
 "use strict";
 
 module.exports = {
 
   create: function(method, url) {
-    var xhr = new XMLHttpRequest()
+    xhr = new XMLHttpRequest()
     xhr.open(method, url, true);
-    self._xhr = xhr;
     return xhr;
   },
 
   send: function (formData) {
-    self._xhr.send(formData);
+    xhr.send(formData);
   },
 
   onProgress: function(callback){
-    self._xhr.upload.onprogress = function (e) {
+    xhr.upload.onprogress = function (e) {
       if (e.lengthComputable) {
         callback(e);
       }
@@ -22,8 +23,8 @@ module.exports = {
   },
 
   onFinish: function(successCallback, errorCallback){
-    self._xhr.onload = function() {
-      if (self._xhr.status === 200) {
+    xhr.onload = function() {
+      if (xhr.status === 200) {
         successCallback();
       } else {
         errorCallback();
