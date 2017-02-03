55b7adad881b924f4338d0d6dea0bf80d9092297
sc0ttj <shout@scottjarvis.com>
2017-02-03 01:08:50 +0000

    fixed: reset scrollmations in cms.saveProgress()

--- src/cms/js/cms.js
+++ src/cms/js/cms.js
@@ -240,6 +240,7 @@ module.exports = {
 
     $html.find('.cms-menu-container, .cms-menu, .cms-modal, .cms-media-btn, .cms-menu-btn').remove();
     $html.find('#cms-init, link[href^="cms"]').remove();
+    $html.find('*').removeClass('scrollmation-image-container-fixed');
     html = $html.html();
 
     // save cleaned up html to localstorage
