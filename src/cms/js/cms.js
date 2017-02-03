6b8162de1116db7f61b89e673f894a22b07e5a8e
sc0ttj <shout@scottjarvis.com>
2017-02-03 01:13:33 +0000

    fix: reset video btns to visible when CMS saves progress and page

--- src/cms/js/cms.js
+++ src/cms/js/cms.js
@@ -194,6 +194,7 @@ module.exports = {
     $html.find('*').removeClass('anim-fade-1s transparent scrollmation-text-js scrollmation-image-container-top scrollmation-image-container-fixed scrollmation-image-container-bottom');
     $html.find('.scrollmation-text').addClass('article');
     $html.find('.video-overlay-button').html('▶');
+    $html.find('.video-overlay-button').removeClass('hidden');
     // get cleaned html
     cleanHTML = $html.html();
 
@@ -244,6 +245,7 @@ module.exports = {
     $html.find('.cms-menu-container, .cms-menu, .cms-modal, .cms-media-btn, .cms-menu-btn').remove();
     $html.find('#cms-init, link[href^="cms"]').remove();
     $html.find('.video-overlay-button').html('▶');
+    $html.find('.video-overlay-button').removeClass('hidden');
     $html.find('*').removeClass('scrollmation-image-container-fixed');
     html = $html.html();
 
