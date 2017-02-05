a949b66aab2c9e1e119436bb66d536ffcb7d5bd8
sc0ttj <shout@scottjarvis.com>
2017-02-05 22:02:28 +0000

    fixed generating preview file paths (server side)

--- src/cms/api/preview.php
+++ src/cms/api/preview.php
@@ -3,9 +3,15 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
+// get root dir of the page being edited (/my-page-name/)
+$url_path = pathinfo(parse_url($_SERVER['HTTP_REFERER'])['path'], PATHINFO_DIRNAME) . '/';
+if ($url_path == '//'){
+  $url_path = parse_url($_SERVER['HTTP_REFERER'])['path'];
+}
+
 // get html from $_POST, save to preview
 $html     = $_POST['html'];
-$filename = $_SERVER['DOCUMENT_ROOT'] . '/' . basename($_SERVER['HTTP_REFERER']) . '/preview.html';
+$filename = $_SERVER['DOCUMENT_ROOT'] . $url_path . 'preview.html';
 
 $handle = fopen($filename,"w");
 if ( fwrite($handle,$html) ){
